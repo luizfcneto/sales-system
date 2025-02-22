fcdb2d6ffef1aaf255305cc0b3d782baae14d943
luizfcneto <luizfcneto123@gmail.com>
2025-02-21 22:46:24 -0300

    +classe de conexão do banco de dados, +ClientService, *Ajuste App

--- src/main/java/com/vsoftware/service/impl/ClientServiceImpl.java
+++ src/main/java/com/vsoftware/service/impl/ClientServiceImpl.java
@@ -1,7 +1,63 @@
 package com.vsoftware.service.impl;
 
+import java.util.List;
+
+import com.vsoftware.dao.ClientDAO;
+import com.vsoftware.dao.impl.ClientDAOImpl;
+import com.vsoftware.domain.Client;
+import com.vsoftware.exception.DatabaseException;
 import com.vsoftware.service.ClientService;
 
 public class ClientServiceImpl implements ClientService {
+	private ClientDAO clientDAO;
+
+    public ClientServiceImpl() {
+        this.clientDAO = new ClientDAOImpl();
+    }
+	
+	@Override
+	public void createClient(Client client) {
+		try {
+            clientDAO.create(client);
+        } catch (DatabaseException e) {
+            throw e;
+        }		
+	}
+
+	@Override
+	public void updateClient(Client client) {
+		try {
+            clientDAO.update(client);
+        } catch (DatabaseException e) {
+            throw e;
+        }		
+	}
+
+	@Override
+	public List<Client> getAllClients() {
+		try {
+            return clientDAO.getAll();
+        } catch (DatabaseException e) {
+            throw e;
+        }
+	}
+
+	@Override
+	public Client getClientByCode(int code) {
+		try {
+            return clientDAO.getClientByCode(code);
+        } catch (DatabaseException e) {
+            throw e;
+        }
+	}
+
+	@Override
+	public void deleteClient(int code) {
+		try {
+            clientDAO.delete(code);
+        } catch (DatabaseException e) {
+            throw e;
+        }		
+	}
 
 }
